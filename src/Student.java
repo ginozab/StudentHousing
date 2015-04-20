5b454c074fd9b5b58ffb8804c778d74742b360e7
Brandon Ginoza <ginozab@aldenv100.allegheny.edu>
2015-04-20 15:34:02 -0400

    source added

---
+++ src/Student.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Student{
+	private String name;
+	private String year;
+	private int fHousing;
+	private int sHousing;
+	private int jHousing;
+	private int fRDN;
+	private int sRDN;
+
+	public void setName(String n) {
+		name = n;
+	}
+
+	public void setYear(String y) {
+		year = y;
+	}
+
+	public void setFH(String fh) {
+		fHousing = fh;
+	}
+
+	public void setSH(String sh) {
+		sHousing = sh;
+	}
+
+	/* public void setJH(String jh) { */
+	/* 	jHousing = jh; */
+	/* } */
+
+	public void setfRDN(String fRD){
+		fRDN = fRD;
+	}
+
+	public void setsRDN(String sRD){
+		sRDN = sRD;
+	}
+
+	public void setjRDN(String jRD){
+		jRDN
+	}
+}
