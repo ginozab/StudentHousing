d87e6461be04ccf4184da0af15ffb94af65f7ab3
Herbert Torrance <torranceh@allegheny.edu>
2015-04-20 15:40:01 -0400

    housing

---
+++ src/Housing.java
@@ -0,0 +1,110 @@
+import java.util.*;
+
+
+
+public class Housing{
+
+public void getPriority(String n){
+
+ // The Weights of the housing in terms of best to worst.
+ // Weights were taken from surveys and therefore represent the student body's opinions.
+ int Nv1 = 1;//North Village 1
+ int Nv2Q = 2;// North village 2 quad
+ int OCH = 3;// Off campus house 
+ int CC = 4; // college court
+ int Nv2D = 5; // north village 2 double
+ int AC = 6;// allegheny commons
+ int WS = 7;// walker single
+ int AH = 8;// allegheny hall
+ int ES = 9;// edwards single
+ int CS = 10;// caflish single
+ int BS = 11;//brooks single
+ int BwD = 12;// baldwin double
+ int CDT = 13;// Caflish double/triple
+ int WD = 14;// walker double
+ int BD = 15;// brooks double
+ int ED = 16;// edwards double
+ int CD = 17;// crawford double
+ int SD = 18;// shultz double
+ int RD = 19;// ravine double
+ 
+	if(n == "Nv1"){
+	return Nv1;
+	}
+	
+	if(n == "Nv2Q"){
+	return Nv2Q;
+	}
+	
+	if(n == "OCH"){
+	return OCH;
+	}
+	
+	if(n == "CC"){
+	return CC;
+	}
+	
+	if(n == "Nv2D"){
+	return Nv2D;
+	}
+
+	if(n == "AC"){
+	return AC;
+	}
+	
+	if(n == "WS"){
+	return WS;
+	}
+	
+	if(n == "AH"){
+	return AH;
+	}
+
+	if(n == "ES"){
+	return ES;
+	}
+	
+	if(n == "CS"){
+	return CS;
+	}
+
+	if(n == "BS"){
+        return BS;
+	}
+
+	if(n = "BwD"){
+	return BwD;
+	}
+
+	if(n == "CDT"){
+	return CDT;
+	}
+	
+	if(n == "WD"){
+	return WD;
+	}
+	
+	if(n == "BD"){
+	return BD;
+	}
+	
+	if(n == "ED"){
+	return ED;
+	}
+	
+	if(n == "CD"){
+	return CD;
+	}
+	
+	if(n == "SD"){
+	return SD;
+	}
+
+	if(n == "RD"){
+	return RD;
+	}	
+
+ }
+ 
+}
+
