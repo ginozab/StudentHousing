62d435cf2c29e50880a5885900faf56a38cde918
Brandon Ginoza <ginozab@aldenv100.allegheny.edu>
2015-04-20 16:14:55 -0400

    source

--- src/Housing.java
+++ src/Housing.java
@@ -4,29 +4,30 @@ import java.util.*;
 
 public class Housing{
 
-public void getPriority(String n){
+public int getPriority(String n){
 
  // The Weights of the housing in terms of best to worst.
  // Weights were taken from surveys and therefore represent the student body's opinions.
- int Nv1 = 1;//North Village 1
- int Nv2Q = 2;// North village 2 quad
- int OCH = 3;// Off campus house 
- int CC = 4; // college court
+ 
+ int Nv1 = 1;  // North Village 1
+ int Nv2Q = 2; // North village 2 quad
+ int OCH = 3;  // Off campus house 
+ int CC = 4;   // college court
  int Nv2D = 5; // north village 2 double
- int AC = 6;// allegheny commons
- int WS = 7;// walker single
- int AH = 8;// allegheny hall
- int ES = 9;// edwards single
- int CS = 10;// caflish single
- int BS = 11;//brooks single
- int BwD = 12;// baldwin double
- int CDT = 13;// Caflish double/triple
- int WD = 14;// walker double
- int BD = 15;// brooks double
- int ED = 16;// edwards double
- int CD = 17;// crawford double
- int SD = 18;// shultz double
- int RD = 19;// ravine double
+ int AC = 6;   // allegheny commons
+ int WS = 7;   // walker single
+ int AH = 8;   // allegheny hall
+ int ES = 9;   // edwards single
+ int CS = 10;  // caflish single
+ int BS = 11;  // brooks single
+ int BwD = 12; // baldwin double
+ int CDT = 13; // Caflish double/triple
+ int WD = 14;  // walker double
+ int BD = 15;  // brooks double
+ int ED = 16;  // edwards double
+ int CD = 17;  // crawford double
+ int SD = 18;  // shultz double
+ int RD = 19;  // ravine double
  
 	if(n == "Nv1"){
 	return Nv1;
@@ -69,7 +70,7 @@ public void getPriority(String n){
 	}
 
 	if(n == "BS"){
-        return BS;
+    return BS;
 	}
 
 	if(n = "BwD"){
